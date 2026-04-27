5082dfa5ca53b07677f625fff13f12ae7dede5f6
cesar2016 <cesars.pro@gmail.com>
2026-04-27 15:29:51 -0300

    FEAT: Add download endpoint for prerendered pages zip

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use ZipArchive;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,4 +28,38 @@ Route::get('/subasta/{slug}', function ($slug) {
     }
     
     abort(404);
+});
+
+Route::get('/download/prerendered', function () {
+    $prerenderedDir = storage_path('app/public/prerendered');
+    
+    if (!file_exists($prerenderedDir)) {
+        return response()->json(['error' => 'No hay archivos pre-renderizados'], 404);
+    }
+    
+    $files = glob("{$prerenderedDir}/*.html");
+    
+    if (empty($files)) {
+        return response()->json(['error' => 'No hay archivos pre-renderizados'], 404);
+    }
+    
+    $zipFile = storage_path('app/public/prerendered.zip');
+    
+    if (file_exists($zipFile)) {
+        unlink($zipFile);
+    }
+    
+    $zip = new ZipArchive();
+    if ($zip->open($zipFile, ZipArchive::CREATE) !== TRUE) {
+        return response()->json(['error' => 'No se pudo crear el ZIP'], 500);
+    }
+    
+    foreach ($files as $file) {
+        $filename = basename($file);
+        $zip->addFile($file, $filename);
+    }
+    
+    $zip->close();
+    
+    return response()->download($zipFile, 'prerendered-pages.zip')->deleteFileAfterSend(true);
 });
